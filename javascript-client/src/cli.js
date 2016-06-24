74a88ed33af75a70a3b07bcb38d87725f4bc497e
neal daniel <portsmith1212@gmail.com>
2016-06-24 15:22:25 -0400

    Changed formatting for client

--- javascript-client/src/cli.js
+++ javascript-client/src/cli.js
@@ -34,11 +34,11 @@ cli
       let command = data.toString().substr(0, 5).toLowerCase()
       let arr = data.toString().split('|')
       if (command === 'msg |') {
-        console.log('[' + arr[1].substr(1, (arr[1].length - 2)) + ']' + ' <' + arr[2].substr(1, (arr[2].length - 2)) + '> says:' + arr[3])
+        this.log('[' + arr[1].substr(1, (arr[1].length - 2)) + ']' + ' <' + arr[2].substr(1, (arr[2].length - 2)) + '>' + arr[3])
       } else if (command === 'con |') {
-        console.log('[' + arr[1].substr(1, (arr[1].length - 2)) + ']' + ' ' + arr[2].substr(1, (arr[2].length - 1)) + ' has joined the chat!')
+        this.log('[' + arr[1].substr(1, (arr[1].length - 2)) + ']' + ' ' + arr[2].substr(1, (arr[2].length - 1)) + ' has joined the chat!')
       } else if (command === 'dis |') {
-        console.log('[' + arr[1].substr(1, (arr[1].length - 2)) + ']' + ' ' + arr[2].substr(1, (arr[2].length - 1)) + ' has left the chat!')
+        this.log('[' + arr[1].substr(1, (arr[1].length - 2)) + ']' + ' ' + arr[2].substr(1, (arr[2].length - 1)) + ' has left the chat!')
       }else {
         this.log(data.toString())
       }
